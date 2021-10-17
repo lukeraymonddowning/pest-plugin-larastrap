f338b1ef0bfcf0fd8a7ebdb86bd3aed4191ebe56
Luke Downing <lukeraymonddowning@gmail.com>
2021-10-17 14:37:18 +0100

    Initial work

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Lukeraymonddowning\Larastrap;
 
+use BadMethodCallException;
 use Pest\Contracts\Plugins\HandlesArguments;
 use Tests\CreatesApplication;
 
@@ -14,7 +15,11 @@ final class Plugin implements HandlesArguments
 {
     public function handleArguments(array $arguments): array
     {
-        (new class {
+        if (!class_exists(CreatesApplication::class)) {
+            throw new BadMethodCallException('You must have a Tests\\CreatesApplication trait available in your Laravel project to use the Larastrap plugin.');
+        }
+
+        (new class() {
             use CreatesApplication;
         })->createApplication();
 
