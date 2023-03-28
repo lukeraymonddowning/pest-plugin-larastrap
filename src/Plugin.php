122122de2422a6be6142577d8db498bb46c71d36
Pedro Gabriel <63373521+PedroGXV@users.noreply.github.com>
2023-03-28 15:30:23 -0300

    Fixed Code Style

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Lukeraymonddowning\Larastrap;
 
-use BadMethodCallException;
 use Pest\Contracts\Plugins\HandlesArguments;
 use Tests\CreatesApplication;
 
@@ -16,7 +15,7 @@ final class Plugin implements HandlesArguments
     public function handleArguments(array $arguments): array
     {
         if (!trait_exists(CreatesApplication::class)) {
-            throw new BadMethodCallException('You must have a Tests\\CreatesApplication trait available in your Laravel project to use the Larastrap plugin.');
+            throw new \BadMethodCallException('You must have a Tests\\CreatesApplication trait available in your Laravel project to use the Larastrap plugin.');
         }
 
         (new class() {
